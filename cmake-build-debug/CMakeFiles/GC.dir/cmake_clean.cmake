f979206636b4d98ec2ccb06cedaf8c030c40a6e9
Mike Zigberman <mikhail.manager@mail.ru>
2021-12-08 22:23:27 +0100

    garbage collector

---
+++ cmake-build-debug/CMakeFiles/GC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GC.dir/main.cpp.o"
+  "GC"
+  "GC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
